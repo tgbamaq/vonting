a5fd264acde2c9797f8b7aca2fbbb5f5174e9bbd
Agustinho <thiago.goncalves@grupobamaq.com.br>
2022-12-26 19:10:15 -0300

    start project

---
+++ app/Http/Controllers/VotingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voting;
+use Illuminate\Http\Request;
+
+class VotingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $voting = Voting::all();
+        return response()->json($voting, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request->validated());
+        return Voting::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Voting  $voting
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Voting $voting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Voting  $voting
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Voting $voting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Voting  $voting
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Voting $voting)
+    {
+        //
+    }
+}
